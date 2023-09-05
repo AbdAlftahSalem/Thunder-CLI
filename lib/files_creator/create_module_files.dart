797ceffbc16f955f875ee4bd012adfb15b1f19d8
AbdAlftahSalem <abd321alftah@gmail.com>
2023-09-06 00:15:45 +0300

    Setup create model

--- lib/files_creator/create_module_files.dart
+++ lib/files_creator/create_module_files.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
 import 'dart:io';
 
 import './create_routes_files.dart';
 import '../consts/const.dart';
+import '../data/remote/base_client.dart';
 import '../extensions/string_extensions.dart';
 import '../services/create_folder_files.dart';
 
 class CreateModuleFiles {
+  bool withModel;
+
+  CreateModuleFiles({this.withModel = false});
+
   void createFiles() {
     stdout.write("Enter your view name [ login / bottom_nav ] : ");
     final name = stdin.readLineSync();
@@ -35,41 +41,120 @@ class CreateModuleFiles {
 
   void _setupFoldersModule(String name) {
     // create modules folder
-    CreateFolderAndFiles().createFolder('app/modules');
+    CreateFolderAndFiles()
+        .createFolder('E:/Flutter new/crypto_new/app/modules');
 
     // create home folder
-    CreateFolderAndFiles().createFolder('app/modules/${name.toLowerCase()}');
+    CreateFolderAndFiles().createFolder(
+        'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}');
 
     // create binding folder
-    CreateFolderAndFiles()
-        .createFolder('app/modules/${name.toLowerCase()}/binding');
+    CreateFolderAndFiles().createFolder(
+        'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/binding');
 
     // controller folder
-    CreateFolderAndFiles()
-        .createFolder('app/modules/${name.toLowerCase()}/controller');
+    CreateFolderAndFiles().createFolder(
+        'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/controller');
 
     // create view folder
-    CreateFolderAndFiles()
-        .createFolder('app/modules/${name.toLowerCase()}/view');
+    CreateFolderAndFiles().createFolder(
+        'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/view');
+
+    // create model folder
+    if (withModel) {
+      CreateFolderAndFiles().createFolder(
+          'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/model');
+    }
   }
 
   void _setupFilesModule(String name) {
     // create home binding file
     CreateFolderAndFiles().createFile(
-      'app/modules/${name.toLowerCase()}/binding/${name.toLowerCase()}_binding.dart',
+      'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/binding/${name.toLowerCase()}_binding.dart',
       ConstStrings.instance.binding(name),
     );
 
     // create home controller file
     CreateFolderAndFiles().createFile(
-      'app/modules/${name.toLowerCase()}/controller/${name.toLowerCase()}_controller.dart',
+      'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/controller/${name.toLowerCase()}_controller.dart',
       ConstStrings.instance.controller(name.toCamelCase()),
     );
 
     // create home view file
     CreateFolderAndFiles().createFile(
-      'app/modules/${name.toLowerCase()}/view/${name.toLowerCase()}_view.dart',
+      'E:/Flutter new/crypto_new/app/modules/${name.toLowerCase()}/view/${name.toLowerCase()}_view.dart',
       ConstStrings.instance.view(name),
     );
+  }
+
+  Future<void> createModelFile() async {
+    // set up model file
+    var data = await _setupRequestData();
+
+    if (data[0]) {
+      BaseClient.safeApiCall(
+        data[1]["url"],
+        data[1]["type"],
+        headers: data[1]["headers"],
+        data: data[1]["body"],
+        queryParameters: data[1]["params"],
+        onSuccess: (data) {
+          print(data);
+        },
+      );
+    }
+  }
+
+  Future _setupRequestData() async {
+    stdout.write(
+        "Enter your model name [ user / product [ thund will add 'model' keyword ] ] : ");
+    final name = stdin.readLineSync();
+
+    if (name == null || name.isEmpty) {
+      print("Name name cannot be empty !!");
+      return (false, null);
+    }
+
+    stdout.write("Enter type of request [ get / post ] : ");
+    RequestType type = _getRequestType(stdin.readLineSync() ?? "");
+
+    stdout.write("Enter your full url : ");
+    final url = stdin.readLineSync();
+
+    if (url == null || url.isEmpty) {
+      print("Url cannot be empty !!");
+      return [false, null];
+    }
+
+    stdout.write("Enter your request body : ");
+    Map body = jsonDecode(stdin.readLineSync() ?? "{}");
+
+    stdout.write("Enter your request headers : ");
+    Map headers = jsonDecode(stdin.readLineSync() ?? "{}");
+
+    stdout.write("Enter your request params : ");
+    Map params = jsonDecode(stdin.readLineSync() ?? "{}");
+
+    return [
+      true,
+      {
+        "name": name,
+        "type": type,
+        "url": url,
+        "body": body,
+        "headers": headers,
+        "params": params,
+      }
+    ];
+  }
+
+  RequestType _getRequestType(String type) {
+    if (type == "get") {
+      return RequestType.get;
+    } else if (type == "post") {
+      return RequestType.post;
+    } else {
+      return RequestType.get;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
 import 'package:args/args.dart';
-
 import 'files_creator/create_module_files.dart';
 import 'files_creator/init_folders.dart';
 
 void main(List<String> arguments) {
   final parser = ArgParser();
 
-  // create flags
-  parser.addFlag('i', help: 'Initialize project with GetX and mvc');
-  parser.addFlag('f',
-      help: 'Initialize Folders and files without installing packages');
-  parser.addFlag('m', help: 'Generate View Controller and Binding files');
-  parser.addFlag('h', help: 'Generate View Controller and Binding files');
+  // Add flags to the argument parser
+  parser
+    ..addFlag('i', help: 'Initialize project with GetX and MVC')
+    ..addFlag('f',
+        help: 'Initialize folders and files without installing packages')
+    ..addFlag('m', help: 'Generate View Controller and Binding files')
+    ..addFlag('mm',
+        help: 'Generate View Controller and Binding files and model')
+    ..addFlag('h', help: 'Show help message');
 
   final results = parser.parse(arguments);
 
   if (results['i']) {
+    // Initialize the project with GetX and MVC
     InitFolders().initFolders();
   } else if (results['m']) {
+    // Generate View Controller and Binding files
     CreateModuleFiles().createFiles();
   } else if (results['f']) {
+    // Initialize folders and files without installing packages
     InitFolders().initFolders(setUpPackage: false);
+  } else if (results['mm']) {
+    // Generate View Controller and Binding files and model
+    CreateModuleFiles(withModel: true).createFiles();
   } else if (results['h']) {
+    // Show help message
     print("Welcome to Thunder CLI 🚀🚀");
     print("Thunder CLI is a tool to generate GetX project files");
     print("Available commands:");
-    print("thunder_cli -i : Initialize project with GetX and mvc");
+    print("thunder_cli -i : Initialize project with GetX and MVC");
     print("thunder_cli -m : Generate View Controller and Binding files");
   } else {
+    // Print usage if no valid flag is provided
     print(parser.usage);
   }
 }
