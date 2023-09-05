5f79be023b157e43e321ed88cf8ad29311c3d304
AbdAlftahSalem <abd321alftah@gmail.com>
2023-09-05 23:35:40 +0300

    Refactor create module

--- lib/files_creator/create_module_files.dart
+++ lib/files_creator/create_module_files.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import './create_routes_files.dart';
 import '../consts/const.dart';
 import '../extensions/string_extensions.dart';
 import '../services/create_folder_files.dart';
@@ -17,27 +18,47 @@ class CreateModuleFiles {
   }
 
   _generateFiles(String name) {
+    // set up modules folder
+    _setupFoldersModule(name);
+
+    // set up files module
+    _setupFilesModule(name);
+
+    // reading routes file
+    RoutesCreator(nameFolder: name).addInRoutesFile();
+
+    // Create routes for module
+    RoutesCreator(nameFolder: name).addInAppRoutesFile();
+
+    print("Create module files successfully 🚀🚀");
+  }
+
+  void _setupFoldersModule(String name) {
     // create modules folder
-    CreateFolderAndFiles()
-        .createFolder('app/modules');
+    CreateFolderAndFiles().createFolder('app/modules');
 
     // create home folder
-    CreateFolderAndFiles().createFolder(
-        'app/modules/${name.toLowerCase()}');
+    CreateFolderAndFiles().createFolder('app/modules/${name.toLowerCase()}');
 
     // create binding folder
-    CreateFolderAndFiles().createFolder(
-        'app/modules/${name.toLowerCase()}/binding');
+    CreateFolderAndFiles()
+        .createFolder('app/modules/${name.toLowerCase()}/binding');
 
+    // controller folder
+    CreateFolderAndFiles()
+        .createFolder('app/modules/${name.toLowerCase()}/controller');
+
+    // create view folder
+    CreateFolderAndFiles()
+        .createFolder('app/modules/${name.toLowerCase()}/view');
+  }
+
+  void _setupFilesModule(String name) {
     // create home binding file
     CreateFolderAndFiles().createFile(
       'app/modules/${name.toLowerCase()}/binding/${name.toLowerCase()}_binding.dart',
       ConstStrings.instance.binding(name),
     );
-
-    // controller folder
-    CreateFolderAndFiles().createFolder(
-        'app/modules/${name.toLowerCase()}/controller');
 
     // create home controller file
     CreateFolderAndFiles().createFile(
@@ -45,130 +66,10 @@ class CreateModuleFiles {
       ConstStrings.instance.controller(name.toCamelCase()),
     );
 
-    // create view folder
-    CreateFolderAndFiles().createFolder(
-        'app/modules/${name.toLowerCase()}/view');
-
     // create home view file
     CreateFolderAndFiles().createFile(
       'app/modules/${name.toLowerCase()}/view/${name.toLowerCase()}_view.dart',
       ConstStrings.instance.view(name),
     );
-
-    // reading routes file
-    _addInRoutes(name);
-
-    // Aad rout in app_routes.dart file
-    _addAppRout(name);
-
-    print("Create module files successfully 🚀🚀");
-  }
-
-  void _addInRoutes(String name) {
-    // create routes file
-
-    // reading routes file
-    final routesFile =
-        File('app/routes/routes.dart');
-
-    // reading routes file content
-    String routesContent = routesFile.readAsStringSync();
-
-    final routesMatch = RegExp(r'static const.*?;')
-        .allMatches(routesContent)
-        .map((e) => e.group(0))
-        .toList();
-
-    // if routesMatch is null, then add routes
-    if (routesMatch.isEmpty) {
-      routesContent += '''
-class Routes {
-  static const ${name.toUpperCase()} = '/${name.toLowerCase()}';
-}
-''';
-    } else {
-      // add routes to content
-
-      routesMatch.add(
-          '  static const ${name.toUpperCase()} = "/${name.toLowerCase()}";');
-      String routes = routesMatch.join('\n');
-      routesContent = '''
-class Routes {
-  $routes
-}
-''';
-    }
-
-    // write content to routes.dart file
-    routesFile.writeAsStringSync(routesContent);
-  }
-
-  void _addAppRout(String name) {
-    // reading app_routes.dart file
-    final file =
-        File('app/routes/app_routes.dart');
-    String content = file.readAsStringSync();
-
-    // extract all imports from code
-    final imports = RegExp(r'import.*?;')
-        .allMatches(content)
-        .map((e) => e.group(0))
-        .toList();
-
-    // extract routes from code
-    final routesMatch =
-        RegExp(r'static final routes = \[([\s\S]*?)\];').firstMatch(content);
-
-    // if import length is 0, then add import
-    if (imports.isEmpty) {
-      content = '''
-import '/app/routes/routes.dart';
-import 'package:get/get.dart';
-import '../modules/${name.toLowerCase()}/binding/${name.toLowerCase()}_binding.dart';
-import '../modules/${name.toLowerCase()}/view/${name.toLowerCase()}_view.dart';
-
-''';
-    } else {
-      content = '''
-    ${imports.join('\n')}
-import '../modules/${name.toLowerCase()}/binding/${name.toLowerCase()}_binding.dart';
-import '../modules/${name.toLowerCase()}/view/${name.toLowerCase()}_view.dart';
-''';
-    }
-
-    // if routesMatch is null, then add routes
-    if (routesMatch == null) {
-      content += '''
-class AppPages {
-  static final routes = [
-    GetPage(
-      name: Routes.${name.toUpperCase()},
-      page: () => ${name.toCamelCase()}View(),
-      binding: ${name.toCamelCase()}Binding(),
-    ),
-  ];
-}
-''';
-    } else {
-// extract routes from routesMatch
-      final routes = routesMatch.group(1);
-
-// add routes to content
-      content += '''
-class AppPages {
-  static final routes = [
-    $routes
-    GetPage(
-      name: Routes.${name.toUpperCase()},
-      page: () => ${name.toCamelCase()}View(),
-      binding: ${name.toCamelCase()}Binding(),
-    ),
-    ];
-}
-''';
-    }
-
-    // write content to app_routes.dart file
-    file.writeAsStringSync(content);
   }
 }
