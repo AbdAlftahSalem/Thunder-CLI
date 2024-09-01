327caa67173adcd85a10dc7d21c64ed03add0205
AbdAlftahSalem <abd321alftah@gmail.com>
2024-09-01 15:02:05 +0300

    Build model api for response 'without test'

--- lib/features/convert_api_collection_to_code/convert_api_collection_to_code.dart
+++ lib/features/convert_api_collection_to_code/convert_api_collection_to_code.dart
@@ -3,8 +3,10 @@ import 'package:thunder_cli/features/convert_api_collection_to_code/extract_requ
 import 'package:thunder_cli/features/convert_api_collection_to_code/get_variables_data.dart';
 import 'package:thunder_cli/features/convert_api_collection_to_code/read_file_path_and_data.dart';
 import 'package:thunder_cli/features/convert_api_collection_to_code/set_routes_in_api_const.dart';
+import 'package:thunder_cli/features/create_api_model/build_model_file.dart';
 
 import '../../core/models/variable_model.dart';
+import '../create_api_model/create_api_model.dart';
 import 'build_body_model_file.dart';
 
 class ConvertApiCollectionToCode {
@@ -30,6 +32,11 @@ class ConvertApiCollectionToCode {
           .firstWhere((element) => element.key.toLowerCase().contains("url"))
           .value,
     );
+
+    // 2- build model for every request
+    for (var element in requests) {
+      await CreateApiModel().createApiModel(requestModelParameter: element);
+    }
 
     // 3- Build model for request has body
     BuildBodyModelFile.buildBodyModelFile(requests);
