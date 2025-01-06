350702ed7cd9ad169da90ff629623e6aae81c94b
AbdAlftahSalem <abd321alftah@gmail.com>
2025-01-06 13:15:05 +0200

    Setup to publish on pub.dev

--- lib/core/consts/folder_paths.dart
+++ lib/core/consts/folder_paths.dart
@@ -10,55 +10,55 @@ class FolderPaths {
 
   FolderPaths._internal();
 
-  String featureFolder = 'E:/Flutter new/dabdob/lib/feature';
+  String featureFolder = 'lib/feature';
   String bindingsFile =
-      'E:/Flutter new/dabdob/lib/core/bindings/bindings_app.dart';
+      'lib/core/bindings/bindings_app.dart';
 
   // routes files
   String appRoutesFile =
-      'E:/Flutter new/dabdob/lib/core/routing/app_routes.dart';
-  String routesFile = 'E:/Flutter new/dabdob/lib/core/routing/routes.dart';
-  String jsonFile = 'E:/Flutter new/dabdob/lib/thunder.json';
+      'lib/core/routing/app_routes.dart';
+  String routesFile = 'lib/core/routing/routes.dart';
+  String jsonFile = 'lib/thunder.json';
 
   String apiConstRoutesFile =
-      "E:/Flutter new/dabdob/lib/helper/constants/api_constants.dart";
+      "lib/helper/constants/api_constants.dart";
 
   String stringConstantPath =
-      "E:/Flutter new/dabdob/lib/helper/constants/strings_constants.dart";
+      "lib/helper/constants/strings_constants.dart";
 
   // feature files
   String folderInFeatures(String name) =>
-      'E:/Flutter new/dabdob/lib/feature/$name';
+      'lib/feature/$name';
 
   String logicFolder(String name) =>
-      "E:/Flutter new/dabdob/lib/feature/$name/logic";
+      "lib/feature/$name/logic";
 
   String uiFolder(String name) =>
-      "E:/Flutter new/dabdob/lib/feature/$name/ui";
+      "lib/feature/$name/ui";
 
   String dataFolder(String name) =>
-      "E:/Flutter new/dabdob/lib/feature/$name/data";
+      "lib/feature/$name/data";
 
   String modelsFolder(String name) =>
-      "E:/Flutter new/dabdob/lib/feature/$name/data/models";
+      "lib/feature/$name/data/models";
 
   String repoFolder(String name) =>
-      "E:/Flutter new/dabdob/lib/feature/$name/data/repo";
+      "lib/feature/$name/data/repo";
 
   String repoFile(String name) =>
-      'E:/Flutter new/dabdob/lib/feature/$name/data/repo/${name.toLowerCase()}_repo.dart';
+      'lib/feature/$name/data/repo/${name.toLowerCase()}_repo.dart';
 
   String controllerFile(String name) =>
-      'E:/Flutter new/dabdob/lib/feature/$name/logic/${name.toLowerCase()}_controller.dart';
+      'lib/feature/$name/logic/${name.toLowerCase()}_controller.dart';
 
   String viewFile(String name) =>
-      'E:/Flutter new/dabdob/lib/feature/$name/ui/${name.toLowerCase()}_view.dart';
+      'lib/feature/$name/ui/${name.toLowerCase()}_view.dart';
 
   String modelFile(String modelName, String featureName) =>
-      'E:/Flutter new/dabdob/lib/feature/${featureName.toLowerCase()}/data/models/${modelName.toLowerCase()}_model.dart';
+      'lib/feature/${featureName.toLowerCase()}/data/models/${modelName.toLowerCase()}_model.dart';
 
   String translationFile(String fileName) =>
-      'E:/Flutter new/dabdob/lib/helper/translations/${fileName}_translation.dart';
+      'lib/helper/translations/${fileName}_translation.dart';
 
-  String locale() => "E:/Flutter new/dabdob/lib/helper/translations/local.dart";
+  String locale() => "lib/helper/translations/local.dart";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'core/services/command_service/command_service.dart';
-import './features/convert_api_collection_to_code/convert_api_collection_to_code.dart';
 
 void main(List<String> arguments) async {
-  // CommandServices.startCommands(arguments);
-  ConvertApiCollectionToCode.convertApiCollectionToCode();
+  CommandServices.startCommands(arguments);
 }
