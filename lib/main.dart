23386b336dd45868b882a8b1292e9002010b6691
AbdAlftahSalem <abd321alftah@gmail.com>
2025-03-06 12:00:39 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'core/services/command_service/command_service.dart';
-import 'features/create_feature/create_feature.dart';
 
 void main(List<String> arguments) async {
-  CreateFeatureFiles.createFiles();
-  // CommandServices.startCommands(arguments);
+  CommandServices.startCommands(arguments);
 }
