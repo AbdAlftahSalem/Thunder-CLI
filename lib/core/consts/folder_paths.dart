2d76187e5dbdc9589d21014d7cf6c07202289ea8
AbdAlftahSalem <abd321alftah@gmail.com>
2025-03-15 09:28:37 +0200

    UdpatE

--- lib/core/consts/folder_paths.dart
+++ lib/core/consts/folder_paths.dart
@@ -8,22 +8,22 @@ class FolderPaths {
 
   FolderPaths._internal();
 
-  String featureFolder = 'E:/Flutter new/test/lib/feature';
-  String bindingsFile = 'E:/Flutter new/test/lib/core/bindings/bindings_app.dart';
+  String featureFolder = 'lib/feature';
+  String bindingsFile = 'lib/core/bindings/bindings_app.dart';
 
   // routes files
-  String appRoutesFile = 'E:/Flutter new/test/lib/core/routing/app_routes.dart';
-  String repoFolder = 'E:/Flutter new/test/lib/app/repositories/';
-  String modelsFolder = 'E:/Flutter new/test/lib/app/models';
-  String uiFolder = 'E:/Flutter new/test/lib/app/ui';
-  String controllersFolder = 'E:/Flutter new/test/lib/app/controllers';
+  String appRoutesFile = 'lib/core/routing/app_routes.dart';
+  String repoFolder = 'lib/app/repositories/';
+  String modelsFolder = 'lib/app/models';
+  String uiFolder = 'lib/app/ui';
+  String controllersFolder = 'lib/app/controllers';
 
-  String routesFile = 'E:/Flutter new/test/lib/core/routing/routes.dart';
-  String jsonFile = 'E:/Flutter new/test/lib/thunder.json';
+  String routesFile = 'lib/core/routing/routes.dart';
+  String jsonFile = 'lib/thunder.json';
 
-  String apiConstRoutesFile = "E:/Flutter new/test/lib/helper/constants/api_constants.dart";
+  String apiConstRoutesFile = "lib/helper/constants/api_constants.dart";
 
-  String stringConstantPath = "E:/Flutter new/test/lib/helper/constants/strings_constants.dart";
+  String stringConstantPath = "lib/helper/constants/strings_constants.dart";
 
   // feature files
   String repoFile(String name) =>
@@ -39,7 +39,7 @@ class FolderPaths {
       '$modelsFolder/${modelName.toLowerCase()}_model.dart';
 
   String translationFile(String fileName) =>
-      'E:/Flutter new/test/lib/helper/translations/${fileName}_translation.dart';
+      'lib/helper/translations/${fileName}_translation.dart';
 
-  String locale() => "E:/Flutter new/test/lib/helper/translations/local.dart";
+  String locale() => "lib/helper/translations/local.dart";
 }
