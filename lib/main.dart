dcbe2e8422f6729f8552cee58ca681ff23127681
AbdAlftahSalem <abd321alftah@gmail.com>
2025-03-15 09:38:39 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:thunder_cli/core/services/command_service/command_service.dart';
-import 'package:thunder_cli/features/init_project/init_project.dart';
 
 void main(List<String> arguments) async {
-  // CommandServices.startCommands(arguments);
-  await InitProject.initProject();
-
+  CommandServices.startCommands(arguments);
 }
